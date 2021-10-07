411955b34205d34a1f85e7a60b3a634581dfa810
matierojo <112119@tecnicatura.frc.utn.edu.ar>
2021-10-07 15:35:48 -0300

    Llenado tablaUsuarios+Popup AltaUsuario+Combo Rol

--- js/conexionApi.js
+++ js/conexionApi.js
@@ -1,12 +1,21 @@
 
 $(document).ready(function() {
+    $('#tablaUsuarios').DataTable({
+        orderCellstop: true,
+        fixedHeader: true
+    });
+});
+
+
+// CARGAMOS TABLA USUARIOS
+$(document).ready(function() {
     $.ajax({
     url: "http://26.100.251.19/api/usuarios/",
     type: "GET",
     headers: {Authorization:"Basic bWF0aWVyb2pvOm1hdGlhc2Vyb2pvMTIz"},
     statusCode: {
         200: function (data) {
-            creartabla(data, $("#cuerpoTabla"));
+            creartabla(data, $("#tablaUsuarios"));
         },
         401: function (data) {
             console.log("error")
@@ -19,16 +28,63 @@ $(document).ready(function() {
 })
 
 function creartabla(datos, tabla){
-    
+    var html = "<thead>";
+        html += "<tr>";
+        html += "<th>Username</th>"
+        html += "<th>Nombre</th>"
+        html += "<th>apellido</th>"
+        html += "<th>email</th>"
+        html += "<th>Rol</th>"
+        html += "</tr>"
+        html += "</thead>"
+        html += "<tbody>"
     for(var i=0; i < datos.length; i++){
-        var html = "<tr>";
-            html += "<td>" + datos[i].username + "</td>";
-            html += "<td>" + datos[i].first_name + "</td>";
-            html += "<td>" + datos[i].last_name + "</td>";
+        
+            html += "<tr>";
+            html += "<td>" + datos[i].usuario + "</td>";
+            html += "<td>" + datos[i].nombre + "</td>";
+            html += "<td>" + datos[i].apellido + "</td>";
             html += "<td>" + datos[i].email + "</td>";
-            html += "<td> Admin </td>";
+            html += "<td>" + datos[i].rol + "</td>";
             html += "<td><div class=\"table-data-feature\">"
-            html += "<button class=\"item \" data-toggle=\"tooltip\" data-placement=\"top\" title=\"Edit\"><i class=\"zmdi zmdi-edit\"></i></button>\<button class=\"item\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"Delete\"><i class=\"zmdi zmdi-delete\"></i></button></div></td>";
-            tabla.append(html);
+            html += "<button class=\"item \" data-toggle=\"modal\" data-placement=\"top\" title=\"Edit\"><i class=\"zmdi zmdi-edit\"></i></button>\<button class=\"item\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"Delete\"><i class=\"zmdi zmdi-delete\"></i></button></div></td>";
+            html += "</tr>";
+            html += "<tr class=\"spacer\"></tr>";
+            
+            
     }
+    html += "</tbody>";
+    tabla.append(html);
+}
+
+// CARGAMOS SELECT CON TIPO ROLES
+
+$.ajax({
+    url: "http://26.100.251.19/api/tipos-rol/", 
+    type: "GET",
+    headers: {Authorization: "Basic bWF0aWVyb2pvOm1hdGlhc2Vyb2pvMTIz"},
+    statusCode: {
+        200: function (data) {
+            console.log(data);
+            cargarSelect(data);
+        },
+        401: function (data) {
+            console.log("error")
+        },
+        500: function (data) {
+            console.log("error")
+        }
+    }
+});
+
+function cargarSelect(data) {
+    data.forEach(function (element) {
+    let miSelect = document.getElementById("selectTipoRol");
+    let opt = document.createElement("option");
+
+    opt.appendChild(document.createTextNode(element.descripcion));
+    opt.value = element.id;
+
+    miSelect.appendChild(opt);
+})
 }
