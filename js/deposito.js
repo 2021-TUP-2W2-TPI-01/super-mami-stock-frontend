ed2ba0f353d4f7198649f4743e75f3aa973bcb1a
1w3112095chaylemaria <69598850+1w3112095chaylemaria@users.noreply.github.com>
2021-10-11 17:32:21 -0300

    Alta deposito con localidades

--- js/deposito.js
+++ js/deposito.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $.ajax({
     url: "http://26.100.251.19/api/depositos/",
     type: "GET",
-    headers: {Authorization:"Basic bWF0aWVyb2pvOm1hdGlhc2Vyb2pvMTIz"},
+    headers: {Authorization:"Basic bWFyaWFjaGF5bGU6bWFyaWExMjM0"},
     statusCode: {
         200: function (data) {
 
@@ -17,7 +17,7 @@ $(document).ready(function() {
                     "infoEmpty": "No hay datos",
                     "infoFiltered": "(Filtrado de _MAX_ total registros)"
                 },
-                pageLength: 5,
+                pageLength: 7,
                 columnDefs: [{
                     'targets': 5,
                     'searchable':false,
@@ -39,12 +39,13 @@ $(document).ready(function() {
 
 function creartabla(datos, tabla){
 
+    datos.forEach(deposito => {
     let botones = '';
     botones += "<td><div class=\"table-data-feature\">"
-    botones += "<button class=\"item \" id=\"btn_delete_deposito\" data-toggle=\"modal\" data-placement=\"top\" title=\"Edit\"><i class=\"zmdi zmdi-edit\"></i></button>\<button class=\"item\" data-toggle=\"modal\" onclick=\confirmar_eliminacion_deposito('"+ deposito.id + "', '" + deposito.nombre + "')\" data-placement=\"top\" title=\"Delete\"><i class=\"zmdi zmdi-delete\"></i></button></div></td>";
+    botones += "<button class=\"item \" id=\"btn_delete_deposito\" data-toggle=\"modal\" data-placement=\"top\" title=\"Modificar\"><i class=\"zmdi zmdi-edit\"></i></button>\<button class=\"item\" data-toggle=\"modal\" onclick=\confirmar_eliminacion_deposito('"+ deposito.id + "', '" + deposito.nombre + "')\" data-placement=\"top\" title=\"Eliminar\"><i class=\"zmdi zmdi-delete\"></i></button></div></td>";
 
-   datos.forEach(deposito => {
-       tabla.row.add([deposito.nombre, deposito.descripcion, deposito.encargado, deposito.domicilio , deposito.barrio, botones]).draw();
+   
+       tabla.row.add([deposito.nombre, deposito.descripcion, deposito.domicilio , deposito.barrio, deposito.localidad, deposito.encargado, botones]).draw();
    });
 
 }
@@ -53,11 +54,11 @@ function creartabla(datos, tabla){
 $.ajax({
     url: "http://26.100.251.19/api/encargados/", 
     type: "GET",
-    headers: {Authorization: "Basic bWF0aWVyb2pvOm1hdGlhc2Vyb2pvMTIz"},
+    headers: {Authorization: "Basic bWFyaWFjaGF5bGU6bWFyaWExMjM0"},
     statusCode: {
         200: function (data) {
-            console.log(data);
-            cargarSelect(data);
+           
+            cargarSelectE(data);
         },
         401: function (data) {
             console.log("error")
@@ -68,18 +69,49 @@ $.ajax({
     }
 });
 
-function cargarSelect(data) {
+function cargarSelectE(data) {
     data.forEach(function (element) {
     let miSelect = document.getElementById("selectEncargado");
     let opt = document.createElement("option");
 
-    opt.appendChild(document.createTextNode(element.nombre));
+    opt.appendChild(document.createTextNode(element.descripcion));
     opt.value = element.id;
 
     miSelect.appendChild(opt);
 })
 }
 
+//CARGAR LOCALIDAD
+
+$.ajax({
+    url: "http://26.100.251.19/api/localidades/", 
+    type: "GET",
+    headers: {Authorization: "Basic bWFyaWFjaGF5bGU6bWFyaWExMjM0"},
+    statusCode: {
+        200: function (data) {
+           
+            cargarSelectL(data);
+        },
+        401: function (data) {
+            console.log("error")
+        },
+        500: function (data) {
+            console.log("error")
+        }
+    }
+});
+
+function cargarSelectL(data) {
+    data.forEach(function (element) {
+    let miSelect = document.getElementById("selectLocalidad");
+    let opt = document.createElement("option");
+
+    opt.appendChild(document.createTextNode(element.descripcion));
+    opt.value = element.id;
+
+    miSelect.appendChild(opt);
+})
+}
 
 // DAR DE BAJA DEPOSITO
 
